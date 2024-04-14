35ddbed292d89b97ac7510472ef7c3ffbcb0418e
Saksham Srivastava <saksham16dec@gmail.com>
2024-04-14 20:41:02 +0530

    removed static deps

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import express from "express";
 import { pool } from "./db";
-import path from "path";
 import cors from "cors";
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 app.get("/", (req: express.Request, res: express.Response) => {
